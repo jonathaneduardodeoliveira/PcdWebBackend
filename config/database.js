29b59eab6063b41a0ed313ad44fe7d79c6210754
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-10-11 17:12:23 -0300

    configurar Sequelize para MySQL

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Conectado ao banco de dados MySQL'))
+    .catch((err) => console.error('Erro ao conectar ao banco de dados:', err));
+
+module.exports = sequelize;
