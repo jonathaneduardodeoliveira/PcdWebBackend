ae762409dacaa5c84e2361427e85631f9358f2aa
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-10-11 17:14:08 -0300

    definir modelo Aluno no Sequelize

---
+++ models/Aluno.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Aluno = sequelize.define('Aluno', {
+    nomeCompleto: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    escolaridade: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    documentos: {
+        type: DataTypes.JSON,
+        allowNull: true
+    },
+    dataCadastro: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: 'alunos'
+});
+
+Aluno.sync({ alter: true })
+    .then(() => console.log('Tabela de alunos sincronizada com o banco de dados'))
+    .catch((err) => console.error('Erro ao sincronizar tabela de alunos:', err));
+
+module.exports = Aluno;
